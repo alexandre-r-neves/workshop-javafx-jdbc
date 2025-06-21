8d3c427f9ec4b3c036b2c62c33ee4308e7628065
Alexandre Neves <alexguit2@hotmail.com>
2025-06-20 22:52:59 -0300

    DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,8 +15,11 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
+	
+	private DepartmentService service;
 	
 	@FXML
 	private TableView<Department> tableViewDepartment;
@@ -28,6 +34,9 @@ public class DepartmentListController implements Initializable {
 	private Button btNew;
 	
 	
+	private ObservableList<Department> obsList;
+	
+	
 	//===========================================================
 	
 	
@@ -37,14 +46,18 @@ public class DepartmentListController implements Initializable {
 	}
 	
 	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+	
+	
 	//===========================================================
 	
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {                            //esse método é o primeiro a ser executado ao abrir a cena
 		
-		initializeNodes();                                                          //chama e executa o método "initializeNodes()" assim que a cena é aberta
-		
+		initializeNodes();                                                          //chama e executa o método "initializeNodes()" assim que a cena é aberta	
 	}
 
 	
@@ -58,6 +71,18 @@ public class DepartmentListController implements Initializable {
 		
 		Stage stage = (Stage) Main.getMainScene().getWindow();                      //acessa a referência do Stage principal
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());      //faz com que a tebleView fique da altura da cena principal (acompanha a altura da cena principal)
+	}
+	
+	
+	public void updateTableView() {                                            //acessa o atributo "serviço", carrega os departamentos e os coloca na "obsList"
+		
+		if (service == null) {                                                 //"estoura" uma excessão caso esqueça de injetar uma dependência para o atributo "Service" ("Service" não instanciado)
+			throw new IllegalStateException("Service was null");
+		}
+		
+		List<Department> list = service.findAll();                             //armazena todos os departamentos que o método "findAll()" da Classe DepartmentService "puxou" do banco de dados 
+		obsList = FXCollections.observableArrayList(list);                     //instancia o obsList e carrega os departamentos da lista "list"
+		tableViewDepartment.setItems(obsList);                                 //carrega os departamentos da "obsList" na tableViewDepartment e os mostra na tela
 		
 	}
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
 
@@ -27,9 +28,9 @@ public class MainViewController implements Initializable {
 	@FXML
 	private MenuItem menuItemAbout;
 	
-	
-	
+		
 	//===================================================
+	
 	
 	@FXML
 	public void onMenuItemSellerAction() {
@@ -38,7 +39,8 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onmenuItemDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml");
+		//loadView("/gui/DepartmentList.fxml");
+		loadView2("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
@@ -47,12 +49,15 @@ public class MainViewController implements Initializable {
 	}
 	
 	
+	//===================================================
 	
 	
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {		
 	}
 	
+	
+	//===================================================
 	
 	
 	private synchronized void loadView(String absoluteName) {        //método para abrir uma nova tela à partir da tela principal (synchronized garante que nada será interrompido durante a multi thread)
@@ -67,6 +72,32 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().clear();                         //limpa todos os filhos do VBox da cena principal
 			mainVBox.getChildren().add(mainMenu);                   //adiciona o mainMenu no VBox da cena principal
 			mainVBox.getChildren().addAll(newVBox.getChildren());   //adiciona os filhos do newVBox no VBox da cena principal
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	
+	//=====================================================================================================================================================================================================
+	
+	
+	private synchronized void loadView2(String absoluteName) {        //método para abrir uma nova tela à partir da tela principal (synchronized garante que nada será interrompido durante a multi thread)
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();                                   //acesso à cena principal
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();  //acesso ao conteúdo do ScrollPane da cena principal
+			
+			Node mainMenu = mainVBox.getChildren().get(0);                  //acessa o primeiro filho do VBox da cena principal
+			mainVBox.getChildren().clear();                                 //limpa todos os filhos do VBox da cena principal
+			mainVBox.getChildren().add(mainMenu);                           //adiciona o mainMenu no VBox da cena principal
+			mainVBox.getChildren().addAll(newVBox.getChildren());           //adiciona os filhos do newVBox no VBox da cena principal
+			
+			DepartmentListController controller = loader.getController();   //acessa o "Controller" da view (acessa a Classe "DepartmentListController" que é a Classe Controller da view DepartmentList.fxml)
+			controller.setDepartmentService(new DepartmentService());       //injeta uma dependência para o atributo "Service" da Classe "DepartmentListController"
+			controller.updateTableView();                                   //"chama" o método "updateTableView()" que contém todos os departamentos do banco de dados
 		}
 		catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
